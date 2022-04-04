391a29c495a0b50930579e74b8774f45cc3934f6
Brody Henry <Brodyhenryftw@gmail.com>
2022-04-04 13:31:55 -0500

    commit

---
+++ Week1/Extra Practice/FirstJavaProgram/Program.java
@@ -0,0 +1,10 @@
+package FirstJavaProgram;
+
+public class Program {
+    public static void main(String[] args) {
+        System.out.println("My Name is Brody Henry");
+        System.out.println("I am 20 years old");
+        System.out.println("I live in Loves Park, IL");
+    }
+
+}
