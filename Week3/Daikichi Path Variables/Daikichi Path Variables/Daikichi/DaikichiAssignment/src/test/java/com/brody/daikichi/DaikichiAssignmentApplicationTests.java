a5a4e724412eb6267b77bc9288aa32a57b4de8bc
BrodyHenry01 <95949715+BrodyHenry01@users.noreply.github.com>
2022-04-22 21:14:04 -0500

    Add files via upload

---
+++ Week3/Daikichi Path Variables/Daikichi Path Variables/Daikichi/DaikichiAssignment/src/test/java/com/brody/daikichi/DaikichiAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brody.daikichi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaikichiAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
